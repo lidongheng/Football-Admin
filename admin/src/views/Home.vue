a75f4e793f9a47ac1080bec6690d4563792f9d11
lidongheng <huoyi-20@163.com>
2019-08-01 23:59:07 +0800

    init1

---
+++ admin/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    home
+  </div>
+</template>
+
+<script>
+import {Component, Vue} from 'vue-property-decorator'
+@Component({
+  components: {
+
+  }
+})
+export default class Home extends Vue {}
+</script>
+
+<style>
+
+</style>
