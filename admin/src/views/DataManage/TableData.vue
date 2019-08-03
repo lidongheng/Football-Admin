e731a7c6cf6fdbe2113529790aa6add077f9eb37
lidongheng <huoyi-20@163.com>
2019-08-03 15:36:57 +0800

    完成侧边栏的动态路由展示

---
+++ admin/src/views/DataManage/TableData.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    404
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component({
+  components: {
+
+  }
+})
+export default class TableData extends Vue {}
+</script>
+
+<style>
+
+</style>
---
+++ admin/src/views/UserManage/AccountData.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    404
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component({
+  components: {
+
+  }
+})
+export default class AccountData extends Vue {}
+</script>
+
+<style>
+
+</style>
