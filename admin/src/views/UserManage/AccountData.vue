48ecc9c56316d8787ecfbc3e57b311b1e4a629f9
lidongheng <huoyi-20@163.com>
2019-08-05 11:03:06 +0800

    完成新增账户功能

--- admin/src/views/UserManage/AccountData.vue
+++ admin/src/views/UserManage/AccountData.vue
@@ -1,17 +1,79 @@
 <template>
-  <div class="home">
-    404
+  <div class="account-data">
+    <div class="add-box">
+      <el-button @click="addAccount" type="primary">新增账户</el-button>
+    </div>
+    <el-table :data="tableData" border style="width:100%">
+      <el-table-column label="角色" width="180">
+        <template slot-scope="scope">
+          <span>{{scope.row.role}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="账号" width="180">
+        <template slot-scope="scope">
+          <span>{{scope.row.username}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="des" label="描述"></el-table-column>
+      <el-table-column label="操作" width="180">
+        <template slot-scope="scope" v-if="scope.row.username != 'admin'">
+          <el-button size="mini">编辑</el-button>
+          <el-button size="mini" type="danger">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <AddAccount :dialogVisible="dialogVisible" :options="options" @closeDialog="closeDialog" @update="getData"></AddAccount>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Provide } from 'vue-property-decorator'
+import AddAccount from './AddAccount.vue'
 @Component({
   components: {
-
+    AddAccount
   }
 })
-export default class AccountData extends Vue {}
+export default class AccountData extends Vue {
+  @Provide() tableData:any = []
+  @Provide() dialogVisible:boolean = false
+  @Provide() options: any = [
+    {
+      key: 'admin',
+      role: '管理员',
+      des: 'Super Administrator. Have access to view all pages.'
+    },
+    {
+      key: 'editor',
+      role: '客服',
+      des: 'Normal Editor. Can see all pages except permission page'
+    },
+    {
+      key: 'visitor',
+      role: '游客',
+      des: 'Just a visitor. Can only see the home page and the document page'
+    }
+  ]
+  addAccount () {
+    this.dialogVisible = true
+  }
+  closeDialog () {
+    this.dialogVisible = false
+  }
+  created () {
+    this.getData()
+  }
+  getData () {
+    (this as any).$axios.get('/api/admin/')
+      .then((res: any) => {
+        console.log(res.data)
+        this.tableData = res.data.users
+      })
+      .catch((err: any) => {
+        console.log(err)
+      })
+  }
+}
 </script>
 
 <style>
---
+++ admin/src/views/UserManage/AddAccount.vue
@@ -0,0 +1,77 @@
+<template>
+  <el-dialog title="新增账户" :visible.sync="dialogVisible" :show-close="false" :close-on-click-modal="false" width="30%">
+    <el-form :rules="rules" ref="ruleForm" :model="account" class="form-box" label-width="100px">
+      <el-form-item label="请选择角色" prop="role">
+        <el-select @change="selectChange" v-model="account.role" placeholder="请选择角色">
+          <el-option v-for="option in options" :label="option.role" :value="option.role" :key="option.key"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="请输入账号" prop="username">
+        <el-input v-model="account.username" placeholder="请输入账号"></el-input>
+      </el-form-item>
+      <el-form-item label="请输入邮箱" prop="email">
+        <el-input v-model="account.email" placeholder="请输入邮箱"></el-input>
+      </el-form-item>
+    </el-form>
+    <span solt="footer" class="dialog-footer">
+      <el-button @click="$emit('closeDialog')">取消</el-button>
+      <el-button @click="handleAdd" type="primary">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Provide, Emit } from 'vue-property-decorator'
+@Component({
+  components: {
+
+  }
+})
+export default class AddAccount extends Vue {
+  @Prop(Boolean) dialogVisible!: boolean
+  @Prop(Array) options!: any
+  @Provide() account: object = {
+    email: '',
+    key: '',
+    role: '',
+    des: '',
+    username: ''
+  }
+  @Provide() rules: any = {
+    username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+    email: [{ required: true, message: '请输入邮箱', trigger: 'blur' }],
+    role: [{ required: true, message: '请输入角色', trigger: 'change' }]
+  }
+  selectChange (select: string) {
+    this.options.map((option: any) => {
+      if (option.role === select) {
+        (this as any).account.key = option.key;
+        (this as any).account.des = option.des
+      }
+    })
+  }
+  @Emit('closeDialog')
+  handleAdd ():void {
+    // @ts-ignore
+    this.$refs['ruleForm'].validate((valid: boolean) => {
+      if (valid) {
+        (this as any).$axios.post('/api/admin/', this.account)
+          .then((res: any) => {
+            this.$emit('update')
+            this.$message({
+              type: 'success',
+              message: res.data.message
+            })
+          })
+          .catch((err: any) => {
+            console.log(err)
+          })
+      }
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
